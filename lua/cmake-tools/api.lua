41164f9be35a7a0018a02be21c5253e2c5c79977
vistdn <tobias.dittmann@vector.com>
2024-05-17 22:44:27 +0200

    Add API to retrieve execution settings

--- lua/cmake-tools/api.lua
+++ lua/cmake-tools/api.lua
@@ -1,4 +1,5 @@
 local config = require("cmake-tools.config")
+local cmake = require("cmake-tools")
 
 M = {}
 
@@ -82,6 +83,29 @@ M.provide_build_settings = function(build_settings)
     end
   end
 
+end
+
+M.retrieve_execution_settings = function()
+  local execution_settings = {}
+
+  local target = cmake.get_launch_target()
+  local target_settings = config.target_settings[target]
+  -- print(target)
+
+  -- retrieve launch path from model
+  local model = config:get_code_model_info()[target]
+  local result = config:get_launch_target_from_info(model)
+  local target_path = result.data
+  local launch_path = vim.fn.fnamemodify(target_path, ":h")
+  -- print(launch_path)
+
+  -- assign data
+  execution_settings["cwd"] = config.base_settings.working_dir
+  execution_settings["exe"] = launch_path .. "/" .. target
+  execution_settings["args"] = target_settings["args"]
+  execution_settings["envs"] = target_settings["env"]
+
+  return execution_settings
 end
 
 return M
